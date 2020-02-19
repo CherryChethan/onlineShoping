1a8d2edb09b125d287e507357373193bf627f3fa
Chetan P <Chetan P@DESKTOP-IG24KUA>
2020-02-19 10:27:34 +0530

    successfull open the html css pages

---
+++ StskShoping/src/main/java/com/stsk/StskShoping/controller/PageController.java
@@ -0,0 +1,36 @@
+package com.stsk.StskShoping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController 
+{
+    @RequestMapping(value= {"/","/home","/index"})
+    public ModelAndView index() {
+    	ModelAndView mv=new ModelAndView("page");
+    	mv.addObject("greeting", "welecome to spring-mvc");
+    	return mv; 
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
